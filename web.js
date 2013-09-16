f8aef500727a916fe71f2cfadac06fc9fae2f446
Ubuntu <ubuntu@ip-172-31-24-189.us-west-2.compute.internal>
2013-09-16 11:38:11 +0000

    web.js corrected

--- web.js
+++ web.js
@@ -1,17 +1,14 @@
-var http = require('http');
-
+var htmlfile = "index.html";
 var express = require ('express');
-
 var app = express.createServer(express.logger());
-
 var fs = require('fs');
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-	throw err;
-    }
+app.get('/', function(request,response) {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+});
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
